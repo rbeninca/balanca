a2e8685f027fc1495401d508a05357f6734780c9
Romulo de Aguiar Beninca <rbeninca@gmail.com>
2026-09-17 00:55:36 -0300

    chore(android): versão 2.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         // TX9 anuncia Android 10 mas roda API 25 (7.1.2)
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
